d901c9614a06dfd7cb49180fde8609d544198405
francivaldo4334 <frncvldreis@gmail.com>
2022-04-09 20:26:05 -0300

    Initial commit

---
+++ app/src/main/java/com/francivaldo/webrequery/MainActivity.java
@@ -0,0 +1,52 @@
+package com.francivaldo.webrequery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    String url;
+    RequestQueue queue;
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edt_text);
+        textView = findViewById(R.id.text_view_respose);
+        // Instantiate the RequestQueue.
+        queue = Volley.newRequestQueue(this);
+        url ="https://my-apis.vercel.app/api/test";
+    }
+
+    public void onButtonClick(View view) {
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
